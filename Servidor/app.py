0648f3ce42d217a7c42d70c6792aecb788b58706
GabrielValeriano <valerianog1205>
2025-08-18 12:06:41 -0300

    creamos una ruta que recibe datos con el metodo POST

--- Servidor/app.py
+++ Servidor/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello; World!</p>"
+
+@app.route("/api/sensores" , methods =["POST"])
+def json_Date():
+    data = request.json
+    nombre = data.get("luxometro")
+    valor = data.get(135)
+    return print(nombre, valor, "ok")
